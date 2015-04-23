bb0c260b3696f6ccd6d3b623ed0ef0f279f26e8d
Robert Kubis <rkubis@google.com>
2015-04-23 13:52:43 +0100

    Updated formatting and added comments
    - Updated index.html and main.css formating
    - Reordered functions in apps.js according to relevance for code challenge
    - Intruduced to pass app object (this) in callback functions to solve context issues
    - Added more descriptive comments for easier integration with Firebase
    - Updated README with instructions
    - Updated LICENSE formatting + adding 2015 Google Inc as Copyright holder
    
    Change-Id: Ie83d98eee23c8497bc542fde06595575ff15d492

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-# 2015 Berlin Speedhack Code Challenge
+# 2015 Berlin Speedhack - Firebase Code Challenge
 
-This repository hosts the scaffold app for the code
-challenge at Speedhack in Berlin as part of the
-APIDays Berlin 2015 conference.
+This repository hosts the scaffold webapp for the Firebase code
+challenge at the 2015 Berlin Speedhack as part of the APIDays conference.
 
 
 ## Get your Toolbox and Workbench ready!
@@ -12,29 +11,27 @@ APIDays Berlin 2015 conference.
   - Editor of your choice
   - Install Gulp and Firebase Tools ```sudo npm install -g gulp firebase-tools```
 
- - After ```git clone <repo-url> && cd <repo-name>``` run ```npm install``` to download all required node packages
+ - Fork the repository and after cloning ```git clone <repo-url> && cd <repo-name>``` run ```npm install```to download all required node packages
  - To start the development server run ```gulp```. It'll start a server on [localhost:8000](http://localhost:8000/).
 
  - You need the following accounts:
-  - GitHub
-  - [Firebase Account](https://www.firebase.com/account/) - Hacker Plan is sufficient for development
+  - Free [GitHub Account](https://github.com/join)
+  - Free [Firebase Account](https://www.firebase.com/account/) - Hacker Plan is sufficient for development
 
 ## Base Camp - Where does everyone start from?
-The app currently works as a single user app. It's built using Twitter Bootstrap and JQuery.
+The application currently works single user only. It's built using Twitter Bootstrap and JQuery.
 You see 5 panel boxes:
  - *Questions on the menu today ;)* - Shows current active question or placeholder if no question is active
- - *You're an admin? Fire your question! :)* - Lets you submit a question that instantly becomes active. Hurry to vote :)
- - *How to participate* - Gives instructions how to participate. This content is currently inactive since only a single user is supported.
- - *#TBT* - Shows the history of previously submitted and voted questions
- - *Voters News Flash* - is a placeholder for multi-user activity updates - currently unused
-
-
+ - *No question active? Fire your question! :)* - Lets you submit a question that instantly becomes active. Hurry to vote :)
+ - *How to participate* - Gives instructions on how to participate. This content is currently inactive since only a single user is supported.
+ - *#TBT - Nostalgia* - Shows the history of previously submitted and voted questions
+ - *Voters News Flash* - is a placeholder for multi-user activity updates - inactive in the single-user scenario
 
 ## The Challenge - Can you solve it?
 Use [Firebase APIs](https://www.firebase.com/how-it-works.html) to finish the following steps:
- 1. Store questions and votes in Firebase and enable real-time synchronization and updates between all clients.
- 2. Integrate an [authentication provider](https://www.firebase.com/docs/web/guide/user-auth.html#section-providers) of your choice (or all if you like and can spare the time), show who is online in the *Voters News Flash* and show the question submit panel only to authenticated users.
- 3. Configure [security & permissions](https://www.firebase.com/docs/web/guide/understanding-security.html#section-authorization) so that only authenticated users can write data.
+ 1. Replace the local data structure ```question: {}``` and store questions and votes in Firebase. Enable real-time synchronization and updates between all clients.
+ 2. Integrate an [authentication provider](https://www.firebase.com/docs/web/guide/user-auth.html#section-providers) of your choice (or all if you like and can spare the time)and show who came online, went inactive or offline in the *Voters News Flash*. Show the question submit panel only to authenticated users.
+ 3. Configure [security & permissions](https://www.firebase.com/docs/web/guide/understanding-security.html#section-authorization) to secure your Firebase and only authenticated users can write data.
 
 To get started checkout the [Firebase Guide](https://www.firebase.com/docs/web/guide/).
 
--- js/app.js
+++ js/app.js
@@ -13,44 +13,49 @@
  * limitations under the License.
  */
 
+
 /*
  * Speed Crowd Voting Application Class
  *
  */
 var app = {
 
-
+  // a submitted question is active for 30 sec
   QUESTION_ACTIVE_TIME_MS: 30000,
 
-  // local sequence number
+  // local sequence number - only used for single user scenario
   questionSeqNumber: 1,
 
+  // local persistence data structure - only used for single user scenario
   questions: {},
+
   historyEmpty: true,
   votersNewsStreamEmpty: true,
   questionActive: false,
 
   /*
    * Initialize the application
-   *
+   * add event listeners here that you want to have available after page load
    */
   init: function() {
     var self = this;
 
     // attach vote event to radio buttons
     $('#voting-buttons input[type=radio]').change(function() {
-        // disable voting radio buttons
-        $('#voting-buttons input[type=radio]').attr('disabled', true);
+      // disable voting radio buttons
+      $('#voting-buttons input[type=radio]').attr('disabled', true);
 
-        // get questionKey
-        var questionKey = $('#active-question').attr('key');
-        // get reference to votes of question object
-        var votesRef = self.questions[questionKey].votes;
-        // push vote
-        votesRef.push($('#voting-buttons input:radio:checked').val());
+      // get questionKey of the active question
+      var questionKey = $('#active-question').attr('key');
 
-        // update Display
-        self.processQuestion(questionKey, self.questions[questionKey]);
+      // get reference to votes of question object
+      var votesRef = self.questions[questionKey].votes;
+
+      // push vote directly to data persistence
+      votesRef.push($('#voting-buttons input:radio:checked').val());
+
+      // update display by fetching the question from persistence and call processQuestion on it
+      self.fetchQuestion(self, questionKey, self.processQuestion);
     });
 
     // set QRCode image in how-to-participate box
@@ -58,33 +63,37 @@ var app = {
 
     // button submit event
     $('#question-submit').click(function() {
-        self.submitNewQuestion()
+      self.submitNewQuestion()
     });
 
     // form submit event
     $('#question-submit-form').submit(function() {
-        self.submitNewQuestion();
-        // avoid default form submit behavior
-        return false;
+      self.submitNewQuestion();
+      // avoid default form submit behavior
+      return false;
     });
   },
 
-  // function to persist a question to the persistence store (in this example a local data structure)
-  // call callback function once finished
-  persistQuestion: function(question, callback) {
+  // function to persist a question to the persistence store (in the scaffold app we use a local data structure)
+  // It is called when a user submits a new question (submitNewQuestion() function)
+  // calls callback function with questionKey and question object once finished
+  persistQuestion: function(self, question, callback) {
+    // get question sequence number (this should be a globally unique number)
+    question.questionNumber = self.questionSeqNumber++;
 
-    // get question sequence number and once obtained persist question and update display
-    question.questionNumber = this.questionSeqNumber++;
-    this.questions[question.questionNumber] = question;
-    callback(question.questionNumber, question);
+    // for the scaffold app we use the questionNumber as the question key. For Firebase use the generated key.
+    var questionKey = question.questionNumber;
+    self.questions[questionKey] = question;
+
+    callback(self, questionKey, question);
   },
 
-  // function to lookup question object by key
-  // call callback function with object if found
-  fetchQuestion: function(questionKey, callback) {
-    // fetch question from Firebase
+  // function to lookup a question by questionkey
+  // calls callback function with questionKey and question object if question was found
+  fetchQuestion: function(self, questionKey, callback) {
+    // fetch question from persistence store
     if (typeof questionKey !== 'undefined') {
-        callback(questionKey, this.questions[questionKey]);
+      callback(self, questionKey, self.questions[questionKey]);
     }
   },
 
@@ -95,116 +104,53 @@ var app = {
 
   /*
    * HINT: no changes required below here for Firebase integration!
+   * You'll only need to use the following two functions (processQuestion and addToVotersNewsStream)
+   * in your Firebase event listeners
    *
+   * processQuestion updates the displayed question (active/history question, update votes)
+   * addToVotersNewsStream adds an entry to the voters activity feed
+   *  it expects the following object structure:
+   *  voter = {
+   *    provider: 'google|twitter|github',
+   *    displayName: 'No Name',
+   *    status: 'online|inactive'
+   *  }
    */
 
-  // read values from form and add question to data structure
-  // trigger update of display
-  submitNewQuestion: function() {
-    // if question text is empty, show error msg and exit
-    if ($('#question').val() == '') {
-        $('#question-submit-error').show();
-        return;
-    }
+  // displays question in active-question-panel if not expired otherwise adds it to #tbt history
+  processQuestion: function(self, questionKey, question) {
+    // needed because Firebase stores scalable arrays as object lists
+    question = helper.transformQuestionVotesToArray(question);
 
-    $('#question-submit-error').hide();
+    // check if question is on air (not yet expired)
+    var questionVotingTimeLeft = (self.QUESTION_ACTIVE_TIME_MS -
+      ($.now() - question.activationTime)) / 1000;
 
-    // create question object
-    question = {
-        questionNumber: -1,
-        question: $('<div/>').text($('#question').val()).html(),
-        activationTime: $.now(),
-        votes: []
-    };
-
-    // clear input
-    $('#question').val('');
-
-    // persist question
-    var self = this;
-    this.persistQuestion(question, function(questionKey, question) {
-      // update display
-      self.processQuestion(questionKey, question);
-    });
-  },
-
-  // update display, move expired questions to #tbt history
-  processQuestion: function(questionKey, question) {
-
-      question = helper.transformQuestionVotesToArray(question);
-
-      // check if question is onAir (not yet expired)
-      var questionVotingTimeLeft = (this.QUESTION_ACTIVE_TIME_MS -
-          ($.now() - question.activationTime)) / 1000;
-
-      if (Math.round(questionVotingTimeLeft) > 0) {
-        this.questionActive = true;
-        // is the question already displayed? then only update the stats
-        var liveQuestionKey = $('#active-question').attr('key');
-        if (typeof liveQuestionKey == 'undefined' || liveQuestionKey != questionKey) {
-          $('#active-question').text("#" + question.questionNumber + " - " + $('<div/>').html(question.question).text());
-          $('#active-question').attr('key', questionKey);
-          $('#time-left').show();
-          this.resetVotingRadioButtons();
-        }
-        // update stats
-        // calculate average rating and update Stats
-        $('#question-avg').text(helper.getAverageOfNumberArray(question.votes));
-        $('#question-vote-count').text(question.votes.length);
-      } else {
-        this.addToQuestionHistory(question);
+    if (Math.round(questionVotingTimeLeft) > 0) {
+      self.questionActive = true;
+      var liveQuestionKey = $('#active-question').attr('key');
+      // is the question already displayed? then only update the stats
+      if (typeof liveQuestionKey == 'undefined' || liveQuestionKey != questionKey) {
+        $('#active-question').text("#" + question.questionNumber + " - " + $('<div/>').html(question.question).text());
+        $('#active-question').attr('key', questionKey);
+        $('#time-left').show();
+        self.resetVotingRadioButtons();
       }
-  },
 
-  // disable / enable admin panel based on auth and if question is currently active
-  toggleEnabledQuestionAdminPanel: function(callback) {
-    // check if user is allowed to see the admin panel
-
-    if (callback()) {
-        $('#question-admin-panel').show();
+      // calculate average rating and update Stats
+      $('#question-avg').text(helper.getAverageOfNumberArray(question.votes));
+      $('#question-vote-count').text(question.votes.length);
     } else {
-        $('#question-admin-panel').hide();
+      self.addToQuestionHistory(self, question);
     }
-
-    // if any question is active -> disable question input and submission
-    $('#question-submit-form :input').prop('disabled', this.questionActive);
   },
 
-  // reset the voting radio buttons
-  resetVotingRadioButtons: function() {
-    // reset radio buttons by clearing prior voting and enable buttons + show info + buttons
-    $('#voting-buttons input[type=radio]').attr('checked', false).attr('disabled', false);
-    $('#voting-buttons-info').show();
-    $('#voting-buttons').show();
-  },
-
-  // add expired question to #tbt history
-  addToQuestionHistory: function(question) {
-    // remove empty history element
-    if (this.historyEmpty) {
-      $('#tbt-question-history li:last').remove();
-      this.historyEmpty = false;
-    }
-
-    $('#tbt-question-history').prepend(
-      '<li><div class="question-container img-rounded panel panel-default"><div class="tbt-question">#' +
-      question.questionNumber + " - " + question.question +
-      '</div><div class="tbt-question-statistics">Average rating: ' +
-      helper.getAverageOfNumberArray(question.votes) + " | " + "votes: " + question.votes.length +
-      '</div><div></li>');
-
-      // only show the last 20 questions, if new are coming in throw the old ones out
-      if ($('#tbt-question-history li').length > 20) {
-        $('#tbt-question-history li:last').remove();
-      }
-  },
-
-  // add voter activity to Voters News Stream - unused in single user scenario
-  addToVotersNewsStream: function(voter, action) {
+  // add voter activity to voters news stream - not used in single user scenario
+  addToVotersNewsStream: function(self, voter, action) {
     // remove placeholder entry
-    if (this.votersNewsStreamEmpty) {
+    if (self.votersNewsStreamEmpty) {
       $('#voters-feed li:last').remove();
-      this.votersNewsStreamEmpty = false;
+      self.votersNewsStreamEmpty = false;
     }
 
     $('#voters-feed').prepend('<li><div class="btn-block btn-sm btn-social btn-' + voter.provider + '">' +
@@ -216,33 +162,110 @@ var app = {
     }
   },
 
-  // function to update timer
+  // read values from form and add question to data structure
+  // trigger update of display
+  submitNewQuestion: function() {
+    var self = this;
+    // if question text is empty, show error msg and exit
+    if ($('#question').val() == '') {
+      $('#question-submit-error').show();
+      return;
+    }
+
+    // hide error if question text is not empty
+    $('#question-submit-error').hide();
+
+    // construct question object
+    question = {
+      questionNumber: -1, // sequence number is set in the persistence function
+      question: $('<div/>').text($('#question').val()).html(),
+      activationTime: $.now(),
+      votes: {}
+    };
+
+    // clear input
+    $('#question').val('');
+
+    // persist question
+    this.persistQuestion(this, question, function(self, questionKey, question) {
+      // callback to processQuestion to update the display
+      self.processQuestion(self, questionKey, question);
+    });
+  },
+
+  // disable / enable admin panel based on auth and if question is currently active
+  // use callback function to determine if user is authenticated
+  toggleEnabledQuestionAdminPanel: function(self, callback) {
+    // check if user is allowed to see the admin panel
+
+    if (callback()) {
+      $('#question-admin-panel').show();
+    } else {
+      $('#question-admin-panel').hide();
+    }
+
+    // if any question is active -> disable question input and submission
+    $('#question-submit-form :input').prop('disabled', self.questionActive);
+  },
+
+  // reset the voting radio buttons
+  resetVotingRadioButtons: function() {
+    // reset radio buttons by clearing prior voting and enable buttons + show info + buttons
+    $('#voting-buttons input[type=radio]').attr('checked', false).attr('disabled', false);
+    $('#voting-buttons-info').show();
+    $('#voting-buttons').show();
+  },
+
+  // add expired question to #tbt history
+  addToQuestionHistory: function(self, question) {
+    // remove empty history element
+    if (self.historyEmpty) {
+      $('#tbt-question-history li:last').remove();
+      self.historyEmpty = false;
+    }
+
+    $('#tbt-question-history').prepend(
+      '<li><div class="question-container img-rounded panel panel-default"><div class="tbt-question">#' +
+      question.questionNumber + " - " + question.question +
+      '</div><div class="tbt-question-statistics">Average rating: ' +
+      helper.getAverageOfNumberArray(question.votes) + " | " + "votes: " + question.votes.length +
+      '</div><div></li>');
+
+    // only show the last 20 questions, if new are coming in throw the old ones out
+    if ($('#tbt-question-history li').length > 20) {
+      $('#tbt-question-history li:last').remove();
+    }
+  },
+
+  // refresh the timer display if question is active and move the question to the history once it's expired
   refreshDisplay: function() {
     if (this.questionActive) {
-      //get the current question in the question-menu-panel
+      //get the current question in the active-question-panel
       var liveQuestionKey = $('#active-question').attr('key');
       var self = this;
-      this.fetchQuestion(liveQuestionKey, function(key, question) {
+      this.fetchQuestion(this, liveQuestionKey, function(self, questionkey, question) {
         if (typeof question !== 'undefined') {
           var questionVotingTimeLeft = (self.QUESTION_ACTIVE_TIME_MS -
             ($.now() - question.activationTime)) / 1000;
+
           if (Math.round(questionVotingTimeLeft) <= 0) {
-              $('#time-left').text("Voting for this question has closed!");
-              $('#voting-buttons input[type=radio]').attr("disabled", true);
-              self.processQuestion(liveQuestionKey, question);
-              self.questionActive = false;
+            $('#time-left').text("Voting for this question has closed!");
+            $('#voting-buttons input[type=radio]').attr("disabled", true);
+            self.processQuestion(self, questionkey, question);
+            self.questionActive = false;
           } else {
-              // Update remaining time to vote
-              $('#time-left').text("Time left to vote: " + Math.round(questionVotingTimeLeft) + " sec");
+            // Update remaining time to vote
+            $('#time-left').text("Time left to vote: " + Math.round(questionVotingTimeLeft) + " sec");
           }
 
-          // show/hide | disable/hide question admin panel
-          self.toggleEnabledQuestionAdminPanel(self.checkIfAuthorized);
+          // show/hide | enable/disable question admin panel
+          self.toggleEnabledQuestionAdminPanel(self, self.checkIfAuthorized);
         }
       });
     }
   },
 };
+
 
 /*
  * Helper Class
@@ -250,28 +273,28 @@ var app = {
  *
  */
 var helper = {
-    getAverageOfNumberArray: function(array) {
-        if (typeof array !== 'undefined') {
-          return array.length > 0 ? (array.reduce(function(a, b) {
-            return a + b;
-            }) / array.length).toPrecision(2) : 0;
-        }
-      return 0;
-    },
-
-    transformQuestionVotesToArray: function(question) {
-      // transform votes into array
-      if (typeof question.votes == 'undefined') {
-        question.votes = [];
-      } else if (typeof question.votes == 'object') {
-        var tempArray = [];
-        for (var key in question.votes) {
-          tempArray.push(parseInt(question.votes[key]));
-        }
-        question.votes = tempArray;
-      }
-      return question;
+  getAverageOfNumberArray: function(array) {
+    if (typeof array !== 'undefined') {
+      return array.length > 0 ? (array.reduce(function(a, b) {
+        return a + b;
+      }) / array.length).toPrecision(2) : 0;
     }
+    return 0;
+  },
+
+  transformQuestionVotesToArray: function(question) {
+    // transform votes into array
+    if (typeof question.votes == 'undefined') {
+      question.votes = [];
+    } else if (typeof question.votes == 'object') {
+      var tempArray = [];
+      for (var key in question.votes) {
+        tempArray.push(parseInt(question.votes[key]));
+      }
+      question.votes = tempArray;
+    }
+    return question;
+  }
 };
 
 
@@ -279,8 +302,10 @@ var helper = {
  * Initialize App
  *
  */
-
 $(document).ready(function() {
   app.init();
-  setInterval(function(){app.refreshDisplay();}, 500);
+  // set refresh interval used to update question timer display
+  setInterval(function() {
+    app.refreshDisplay();
+  }, 500);
 });
